e67e6b56c4e2ca72cc35f8446bc67937a8c1a452
Giordano Temple <giordanotemple@gmail.com>
2023-01-17 13:34:11 +0000

    assignment complete

--- survey.js
+++ survey.js
@@ -1,12 +1,40 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('What do you think of Node.js? ', (answer) => {
-  console.log(`Thank you for your valuable feedback: ${answer}`);
-
-  rl.close();
-});
+rl.question(
+  `What's your name? Nicknames are also acceptable :) `,
+  (nickname) => {
+    rl.question(`What's an activity you like doing? `, (activity) => {
+      rl.question(`What do you listen to while doing that? `, (listen) => {
+        rl.question(
+          `Which meal is your favourite (eg: dinner, brunch, etc.) `,
+          (meal) => {
+            rl.question(
+              `What's your favourite thing to eat for that meal? `,
+              (food) => {
+                rl.question(
+                  `Which sport is your absolute favourite? `,
+                  (sport) => {
+                    rl.question(
+                      `What is your superpower? In a few words, tell us what you are amazing at! `,
+                      (superpower) => {
+                        console.log(
+                          `Hi, I'm ${nickname}! I like ${activity} while listening to ${listen}. My favourite meal is ${meal} because I can eat ${food}. I also enjoy ${sport}. Finally, my superpower is: ${superpower}.`
+                        );
+                        rl.close();
+                      }
+                    );
+                  }
+                );
+              }
+            );
+          }
+        );
+      });
+    });
+  }
+);
